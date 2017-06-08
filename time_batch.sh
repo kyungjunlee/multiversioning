383be69599a6cd0f456373534d9bfe36418d0265
Stan <stanislaw.swidwinski@yale.edu>
2017-06-08 14:20:15 -0400

    Naming conentions changes

--- time_batch.sh
+++ time_batch.sh
@@ -1,5 +1,5 @@
-mkdir -p timing_stuff
-mkdir -p timing_stuff/data
+mkdir -p time_whole_system
+mkdir -p time_whole_system/data
 
 source experiment_config
 
@@ -12,9 +12,9 @@ for batch_size_indicator in `seq 0 $((${#batch_sizes[@]} - 1))`;
       exec_threads=${exec_array[$threads_indicator]}
       batch_size=${batch_sizes[$batch_size_indicator]}
 
-			numactl --interleave=all perf record -a -g build/batch_db --num_txns $num_txns --exp_reps $exp_reps --output_dir timing_stuff/data --batch_size $batch_size --num_sched_threads $sched_threads --num_exec_threads $exec_threads --num_records $db_recs --avg_shared_locks $shared_lock_num --std_dev_shared_locks $shared_std_dev --avg_excl_locks $excl_lock_num --std_dev_excl_locks $excl_std_dev; 
+			numactl --interleave=all perf record -a -g build/batch_db --num_txns $num_txns --exp_reps $exp_reps --output_dir time_whole_system/data --batch_size $batch_size --num_sched_threads $sched_threads --num_exec_threads $exec_threads --num_records $db_recs --avg_shared_locks $shared_lock_num --std_dev_shared_locks $shared_std_dev --avg_excl_locks $excl_lock_num --std_dev_excl_locks $excl_std_dev; 
 
 			perf script | FlameGraph/stackcollapse-perf.pl > FlameGraph/out.perf-folded
-			FlameGraph/flamegraph.pl FlameGraph/out.perf-folded > timing_stuff/time_perf_$sched_threads.$exec_threads.$batch_size.svg 
+			FlameGraph/flamegraph.pl FlameGraph/out.perf-folded > time_whole_system/time_perf_$sched_threads.$exec_threads.$batch_size.svg 
 	done; 
 done;
