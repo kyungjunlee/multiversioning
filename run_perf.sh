5f9d8f4a4a5195e00fa87e958947eb5dd7f6d7cc
Kyungjun Lee <kyungjun.kj.lee@gmail.com>
2017-11-09 16:52:20 -0500

    created our new branch and added new profiling scripts

---
+++ run_perf.sh
@@ -0,0 +1,20 @@
+mkdir -p data
+
+source experiment_config
+
+make batch;
+
+for threads_indicator in `seq 0 $((${#exec_array[@]} - 1))`; 
+  do for batch_size_indicator in `seq 0 $((${#batch_sizes[@]} - 1))`;
+    do 
+      sched_threads=${sched_array[$threads_indicator]}
+      exec_threads=${exec_array[$threads_indicator]}
+      batch_size=${batch_sizes[$batch_size_indicator]}
+  
+      perf record -e major-faults -e minor-faults -e faults-ag \
+      numactl --interleave=all build/batch_db --num_txns $num_txns --output_dir data --batch_size $batch_size --num_sched_threads $sched_threads --num_exec_threads $exec_threads --num_records $db_recs --avg_shared_locks $shared_lock_num --std_dev_shared_locks $shared_std_dev --avg_excl_locks $excl_lock_num --std_dev_excl_locks $excl_std_dev --num_table_merging_shard $num_table_merging_shard; 
+
+      mv perf.data perf-${exec_threads}-${sched_threads}-${batch_size}.data
+
+  done; 
+done;
